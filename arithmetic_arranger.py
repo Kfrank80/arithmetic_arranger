a64f29a0e128c6deb9ca0c4aa7252f7b3f3db626
Frank E. Ramirez <frankernesto.ramirez@gmail.com>
2023-05-26 01:04:26 -0400

    Created file arithmetic_arranger with arithmetic_arranger function

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,11 +1,4 @@
-from typing import Union, Any
-
-
-
-
-
-
-def arithmetic_arranger(arithmetic_problems, show_answers=False) -> Union[str, Any]:
+def arithmetic_arranger(arithmetic_problems, show_answers=False) -> str:
     """
 Create a function that receives a list of strings that are arithmetic problems
 and returns the problems arranged vertically and side-by-side.
@@ -14,29 +7,31 @@ When the second argument is set to True, the answers should be displayed.
     :return:
     """
     first_line = second_line = third_line = fourth_line = f''
-    result = 0
     arith_prob = list(arithmetic_problems)
     if len(arith_prob) > 5:
         return 'Error: Too many problems.'
-    for it in range(0, len(arith_prob)):
-        next_problem = str(arith_prob[it]).split()
-        if next_problem[1] != '+' and next_problem[1] != '-':
-            return f'Error: Operator must be \'+\' or \'-\'.'
-        elif int(next_problem[0]) > 9999 or int(next_problem[2]) > 9999:
-            return f'Error: Numbers cannot be more than four digits.'
-        else:
-            first_operand = int(next_problem[0])
-            second_operand = int(next_problem[2])
-            if str(next_problem[1]) == '+':
-                resul = first_operand + second_operand
+    try:
+        for it in range(0, len(arith_prob)):
+            next_problem = str(arith_prob[it]).split()
+            if next_problem[1] != '+' and next_problem[1] != '-':
+                return f'Error: Operator must be \'+\' or \'-\'.'
+            elif int(next_problem[0]) > 9999 or int(next_problem[2]) > 9999:
+                return f'Error: Numbers cannot be more than four digits.'
             else:
-                result = first_operand - second_operand
-        first_line += f'    {" "*(2 + (len(next_problem[0]) if int(next_problem[0]) > int(next_problem[2]) else len(next_problem[2])) - len(next_problem[0]))}{next_problem[0]}'
-        second_line += f'    {next_problem[1]}{" "*(1 + (len(next_problem[0]) if int(next_problem[0]) > int(next_problem[2]) else len(next_problem[2])) - len(next_problem[2]))}{next_problem[2]}'
-        third_line += f'    {"-"*(2 + (len(next_problem[0]) if int(next_problem[0]) > int(next_problem[2]) else len(next_problem[2])))}'
-        if show_answers == True:
-            # TODO: Arreglar la cuarta linea que no imprime correctmente
-            fourth_line += f'    {" "*(2 + (len(next_problem[0]) if int(next_problem[0]) > int(next_problem[2]) else len(next_problem[2])) - len(str(result)))}{str(result)}'
+                first_operand = int(next_problem[0])
+                second_operand = int(next_problem[2])
+                if str(next_problem[1]) == '+':
+                    result = first_operand + second_operand
+                else:
+                    result = first_operand - second_operand
+            first_line += f'    {" " * (2 + (len(next_problem[0]) if int(next_problem[0]) > int(next_problem[2]) else len(next_problem[2])) - len(next_problem[0]))}{next_problem[0]}'
+            second_line += f'    {next_problem[1]}{" " * (1 + (len(next_problem[0]) if int(next_problem[0]) > int(next_problem[2]) else len(next_problem[2])) - len(next_problem[2]))}{next_problem[2]}'
+            third_line += f'    {"-" * (2 + (len(next_problem[0]) if int(next_problem[0]) > int(next_problem[2]) else len(next_problem[2])))}'
+            if show_answers:
+                fourth_line += f'    {" " * (2 + (len(next_problem[0]) if int(next_problem[0]) > int(next_problem[2]) else len(next_problem[2])) - len(str(result)))}{str(result)}'
+    except ValueError:
+        return f'Error: Numbers must only contain digits.'
+    except IndexError:
+        return f'Error: Must enter at least one exercise.'
 
-    return print(first_line + '\n' + second_line + '\n' + third_line + '\n' + fourth_line)
-
+    return first_line + '\n' + second_line + '\n' + third_line + '\n' + fourth_line
--- main.py
+++ main.py
@@ -1,14 +1,8 @@
-# This is a sample Python script.
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 import arithmetic_arranger
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    arithmetic_arranger.arithmetic_arranger(["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"], True)
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print(arithmetic_arranger.arithmetic_arranger([''], True))
